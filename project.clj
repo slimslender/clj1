1e9e58b04c6dcb99f7f3e73fc5173f92a557e0f8
Jim Clark <slimslenderslacks@gmail.com>
2018-05-30 22:38:56 -0700

    .

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
- (defproject clj2 "0.1.0-SNAPSHOT"
+ (defproject clj1 "0.1.0-SNAPSHOT"
    :description "IXME: write description"
    :dependencies [[org.clojure/clojure "1.9.0"]
                   [metosin/compojure-api "1.1.11"]]
