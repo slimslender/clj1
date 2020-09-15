b7ee18895a8983097f194cc4d9ae8c61a975ed88
Jim Clark <slimslenderslacks@gmail.com>
2020-09-15 12:03:19 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
- (defproject clj1 "0.1.3-SNAPSHOT"
+ (defproject clj1 "0.1.4-SNAPSHOT"
    :description "IXME: write description"
    :dependencies [[org.clojure/clojure "1.10.0"]
                   [metosin/compojure-api "1.1.13" :exclusions [commons-codec com.fasterxml.jackson.core/jackson-core]]
